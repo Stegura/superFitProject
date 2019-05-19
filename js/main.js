7fb17210fbe53b941a16ef009b8fbd38124b362f
Stegura <danylostegyra@gmail.com>
2019-05-19 23:47:03 +0300

    add count down timer to gallery section, and add btn for form popup in gallery section

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// CountDowm timer
+
+// Set the hours minutes and second HTML elements
+let h = document.getElementById('h'),
+    m = document.getElementById('m'),
+    s = document.getElementById('s');
+
+var countDownDate = new Date("may 30, 2019 23:14:55").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    if (hours < 10) {
+        h.textContent = `0${hours}`;
+    } else {
+        h.textContent = hours;
+    }
+
+    
+    if (minutes < 10) {
+        m.textContent = `0${minutes}`;
+    } else {
+        m.textContent = minutes;
+    }
+
+    
+    if (seconds < 10) {
+        s.textContent = `0${seconds}`;
+    } else {
+        s.textContent = seconds;
+    }
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+    clearInterval(x);
+    h.textContent = '00';
+    m.textContent = '00';
+    s.textContent = '00';
+    }
+}, 1000);
